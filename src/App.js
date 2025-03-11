98fb0050d80b7a2343bc3ef568bab5f404d4cf04
4DerekWang <derekwang0919@gmail.com>
2025-03-11 18:34:58 -0400

    finished sidebar and layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.scss';
 import {Routes, Route} from 'react-router-dom'
 import Layout from './components/Layout'
-import Sidebar from './components/Sidebar'
+import Home from './components/Home'
+// import About from './components/About'
+// import Contact from './components/Contact'
+// import Portfolio from './components/Portfolio'
+// import Dashboard from './components/Dashboard'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />} />
+      <Route path = "/" element = {<Layout />}>
+        <Route index element={<Home />} />
+        {/*<Route path = "about" element={<About />} />
+        <Route path = "/contact" element={<Contact />} />
+        <Route path = "/portfolio" element={<Portfolio />} />
+        <Route path = "/dashboard" element={<Dashboard />} /> */}
+      </Route>
     </Routes>
     </>
   );
